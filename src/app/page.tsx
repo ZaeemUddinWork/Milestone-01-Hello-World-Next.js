e0cd2f663c0842700f330335b67c22c49951ab66
ZaeemUddinWork <zaeemuddinbus@gmail.com>
2024-11-05 17:36:55 +0500

    Milestone 01

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+    <div className="text-center">
+      <h1 className=" font-itim-regular text-4xl lg:text-6xl xl:text-7xl 2xl:text-8xl ">Hello World</h1>
+    </div>
+  );
+}
